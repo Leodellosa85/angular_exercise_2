0e99d6cd9aa69514f6b24e1c76d831271ced138e
Leo Dellosa <leodellosa85@gmail.com>
2024-07-21 00:01:13 +0800

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogItemComponent } from './blog/components/blog-item/blog-item.component';
+import { BookItemComponent } from './book/components/book-item/book-item.component';
+import { UserModule } from './user/user.module';
+import { FormComponent } from './user/components/form/form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'blog',
+    pathMatch: 'prefix',
+  },
+  {
+    path: 'blog',
+    component: BlogItemComponent
+  },
+
+  {
+    path: 'book',
+    component: BookItemComponent,
+  },
+  
+  {
+    path: 'user',
+    component: FormComponent
+
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
